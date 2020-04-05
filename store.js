d6c54a38d54abb3da92f2073fb2c8bd97df23868
Maciej Kowalczyk <maciej98kowalczyk@gmail.com>
2020-04-05 19:38:26 +0200

    Ponawianie operacji w tabeli

--- store.js
+++ store.js
@@ -2,6 +2,9 @@ const storage = require('azure-storage')
 const service = storage.createTableService()
 const table = 'tasks'
 
+var retryOperations = new azure.ExponentialRetryPolicyFilter();
+var tasks = azure.createTableService().withFilter(retryOperations);
+
 const init = async () => (
   new Promise((resolve, reject) => {
     service.createTableIfNotExists(table, (error, result, response) => {
